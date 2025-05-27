cee58c5e5d375510c70050266cc6811fec752e84
Pattaradanai Srichon <aprils96950@gmail.com>
2025-05-27 12:48:07 +0700

    add QuestItem controller test and run the passed test

--- app/controllers/quest_items_controller.rb
+++ app/controllers/quest_items_controller.rb
@@ -1,13 +1,43 @@
 class QuestItemsController < ApplicationController
+  before_action :set_quest_item, only: [ :update, :destroy ]
+
   def index
   end
 
   def new
+    @quest_item = QuestItem.new
+
+    render :new
   end
 
   def create
+    @quest_item = QuestItem.new(quest_item_params)
+
+    if @quest_item.save
+      redirect_to quest_items_path, notice: "Quest item was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @quest_item.update(quest_item_params)
+      redirect_to @quest_item, notice: "Quest item was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def quest_item_params
+    params.require(:quest_item).permit(:name, :completed, :id)
+  end
+
+  def set_quest_item
+    @quest_item = QuestItem.find(params[:id])
   end
 end
--- spec/requests/quest_items_spec.rb
+++ spec/requests/quest_items_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "QuestItems", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/quest_items/index"
+      get "/quest_items"
       expect(response).to have_http_status(:success)
     end
   end
@@ -15,18 +15,29 @@ RSpec.describe "QuestItems", type: :request do
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/quest_items/create"
-      expect(response).to have_http_status(:success)
+  describe "POST /create" do
+    let(:valid_attributes) { { name: "Added New Quest Item", completed: false } }
+
+    it "creates a new quest item" do
+      expect {
+        post quest_items_path, params: { quest_item: valid_attributes }
+      }.to change(QuestItem, :count).by(1)
+      expect(response).to redirect_to(quest_items_path)
     end
   end
 
-  describe "GET /delete" do
-    it "returns http success" do
-      get "/quest_items/delete"
-      expect(response).to have_http_status(:success)
+  describe "PATCH /update" do
+    let(:quest_item) { create(:quest_item) }
+
+    it "changes the completed status" do
+      patch quest_item_path(quest_item), params: { quest_item: { completed: true } }
+      quest_item.reload
+      expect(quest_item.completed).to be true
     end
   end
 
+  describe "DELETE /destroy" do
+    it "returns http success" do
+    end
+  end
 end
